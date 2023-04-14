4e7f6cd4d46b5a4bd260ea7a3f3e0c993fca9c20
pawan suthar <pawansuthar15372097023627153@gmail.com>
2023-04-14 15:48:35 +0530

    add useparams to get data from url and print

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const UserDetails = () => {
+    const {usrid} = useParams()
+    
   return (
     <div>
-        <h1>user details here </h1>
+        <h1>details about user {usrid}  </h1>
       
     </div>
   )
