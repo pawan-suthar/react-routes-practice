7f1c942b9904c90247919094feb85d0065ccda08
pawan suthar <pawansuthar15372097023627153@gmail.com>
2023-04-14 14:26:05 +0530

    navigate programtically using useNavigate hook

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {Routes, Route} from "react-router-dom"
 import Home from "./components/Home"
 import About from "./components/About"
 import Navbar from './components/Navbar'
+import Ordercomplete from "./components/Ordercomplete"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='about' element={<About/>}/>
+      <Route path='ordercmp' element={<Ordercomplete/>}/>
     </Routes>
     </>
 
