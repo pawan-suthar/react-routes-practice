4b6e76271facb5503bcec4604f8c103cff257329
pawan suthar <pawansuthar15372097023627153@gmail.com>
2023-04-14 14:26:34 +0530

    navigate progmticly uing usenavigate hook

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const navigate = useNavigate()
   return (
     <div>
         <h1> home component</h1>
+        <button onClick={() => navigate('ordercmp')}>click to complete order</button>
       
     </div>
   )
---
+++ src/components/Ordercomplete.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Ordercomplete = () => {
+  return (
+    <div>
+        <h1> order completed</h1>
+      
+    </div>
+  )
+}
+
+export default Ordercomplete
